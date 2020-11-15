ed25ae88f6ad6e539b6b591b1a3e389069339617
Camilo Paez <cepn26@gmail.com>
2020-11-15 15:49:03 -0500

    v1.0

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 let input;
-let split;
 let textPrint = document.getElementById("text-result");
 let toDelete;
-let splitDelete;
-let resultado;
 
 const censurar = () => {
   toDelete = document.getElementById("text-words").value
@@ -11,50 +8,50 @@ const censurar = () => {
 
   if (input.length > 0) {
     toDelete = toDelete.toLowerCase()
-    splitDelete = toDelete.split(" ")
+    toDelete = toDelete.split(" ")
     input = input.toLowerCase();
-    split = input.split(" ");
-    split.forEach((currentValue, index) => {
+    input = input.split(" ");
+    input.forEach((currentValue, index) => {
       switch (currentValue) {
 
-        case splitDelete[0]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[0]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[1]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[1]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[2]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[2]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[3]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[3]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[4]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[4]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[5]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[5]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[6]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[6]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[7]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[7]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[8]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[8]:
+          input[index] = "*".repeat(currentValue.length);
           break;
 
-        case splitDelete[9]:
-          split[index] = "*".repeat(currentValue.length);
+        case toDelete[9]:
+          input[index] = "*".repeat(currentValue.length);
           break;
       }
     });
@@ -62,7 +59,7 @@ const censurar = () => {
   else {
     textPrint.innerHTML = "No escribiste nada"
   }
-  resultado = split.join(" ")
+  let resultado = input.join(" ")
   textPrint.textContent = resultado
 };
 
