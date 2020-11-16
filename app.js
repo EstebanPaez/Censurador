69581cf8adf178df55540e16f66e575254d3c903
Camilo Paez <cepn26@gmail.com>
2020-11-15 19:11:49 -0500

    v1.1

--- app.js
+++ app.js
@@ -1,66 +1,46 @@
-let input;
 let textPrint = document.getElementById("text-result");
+let input;
 let toDelete;
 
 const censurar = () => {
+  document.querySelector("body").classList.add("fixed")
+  document.querySelector(".container-result").classList.add("active")
   toDelete = document.getElementById("text-words").value
   input = document.getElementById("text").value;
 
   if (input.length > 0) {
+
     toDelete = toDelete.toLowerCase()
     toDelete = toDelete.split(" ")
     input = input.toLowerCase();
     input = input.split(" ");
+
     input.forEach((currentValue, index) => {
-      switch (currentValue) {
-
-        case toDelete[0]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[1]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[2]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[3]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[4]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[5]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[6]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[7]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[8]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-
-        case toDelete[9]:
-          input[index] = "*".repeat(currentValue.length);
-          break;
-      }
+      toDelete.forEach((element) => {
+        if (currentValue.startsWith(element) && currentValue.endsWith(",")) {
+          input[index] = "$".repeat(currentValue.length - 1) + ","
+        }
+        else if (currentValue.startsWith(element) && currentValue.endsWith(".")) {
+          input[index] = "$".repeat(currentValue.length - 1) + "."
+        }
+        else if (currentValue.startsWith(element)) {
+          input[index] = "$".repeat(currentValue.length)
+        }
+      });
     });
   }
   else {
     textPrint.innerHTML = "No escribiste nada"
   }
-  let resultado = input.join(" ")
+  resultado = input.join(" ")
   textPrint.textContent = resultado
 };
 
+
 document.getElementById("go").addEventListener("click", censurar)
+document.querySelector(".up").addEventListener("click", () => window.scrollTo({ top: 0, behavior: 'smooth' }))
+
+document.querySelector(".clear").addEventListener("click", () => {
+  document.querySelector(".container-result").classList.remove("active");
+  document.querySelector("body").classList.remove("fixed")
+})
